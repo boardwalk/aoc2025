34c0bd7a0508a7fded7701dfe22d795c168cc890
Dan Skorupski <boardwalk@ersatsz.com>
2025-12-02 21:35:09 -0600

    day02 part 2

--- crates/day02/src/main.rs
+++ crates/day02/src/main.rs
@@ -1,27 +1,59 @@
 use anyhow::{Error, anyhow};
-use std::ops::RangeInclusive;
+use std::collections::BTreeSet;
 
-// search in an range of ids for invalid ids, appending them onto `out`
-fn find_invalid_ids(ids: RangeInclusive<i64>, out: &mut Vec<i64>) {
-    for id in ids {
-        let id_str = id.to_string();
+const IS_PART_TWO: bool = true;
+
+fn is_invalid_id(id: i64) -> bool {
+    let id_str = id.to_string();
+
+    if IS_PART_TWO {
+        let mut sequence_len = 1;
+        while sequence_len <= id_str.len() {
+            if id_str.len() % sequence_len != 0 {
+                // id string doesn't chunk evenly at this sequence_len
+                sequence_len += 1;
+                continue;
+            }
+
+            let mut begin: usize = 0;
+            let mut all_match = true;
+            let mut num_matches: usize = 0;
+
+            while begin + sequence_len <= id_str.len() {
+                let left = &id_str[0..sequence_len];
+                let right = &id_str[begin..begin + sequence_len];
+
+                if left != right {
+                    all_match = false;
+                    break;
+                }
+                num_matches += 1;
+
+                begin += sequence_len;
+            }
+
+            if all_match && num_matches >= 2 {
+                return true;
+            }
+
+            sequence_len += 1;
+        }
+
+        false
+    } else {
         if id_str.len() % 2 != 0 {
             // odd number of digits
-            continue;
+            return false;
         }
 
         let (left, right) = id_str.split_at(id_str.len() / 2);
 
-        if left == right {
-            out.push(id);
-        }
-
-        // println!("{id}");
+        left == right
     }
 }
 
 fn main() -> Result<(), Error> {
-    let mut invalid_ids = Vec::new();
+    let mut invalid_ids = BTreeSet::new();
 
     for line in std::io::stdin().lines() {
         let line = line?;
@@ -34,16 +66,19 @@ fn main() -> Result<(), Error> {
             let first = i64::from_str_radix(&range[0..sep], 10)?;
             let last = i64::from_str_radix(&range[sep + 1..], 10)?;
 
-            find_invalid_ids(first..=last, &mut invalid_ids);
+            for id in first..=last {
+                if is_invalid_id(id) {
+                    invalid_ids.insert(id);
+                }
+            }
         }
-
-        println!("{line}");
     }
 
-    // println!("{invalid_ids:?}");
+    println!("{invalid_ids:?}");
 
-    let tot: i64 = invalid_ids.iter().sum();
-    println!("tot = {tot}");
+    let tot_invalid: i64 = invalid_ids.iter().sum();
+
+    println!("tot_invalid = {tot_invalid}");
 
     Ok(())
 }
